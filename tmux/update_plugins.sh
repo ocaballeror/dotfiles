28f05920be604931f34de8cead2a05edb6701dec
ocaballeror <ocaballeror@gmail.com>
2017-07-03 23:55:42 +0200

    Added tmux yank + support for other tmux plugins

---
+++ tmux/update_plugins.sh
@@ -0,0 +1,16 @@
+tmuxdir="$HOME/.tmux.d"
+[ -n "$TMUX_DIR" ] && tmuxdir="$TMUXDIR"
+
+[ ! -d "$tmuxdir" ] && mkdir "$tmuxdir"
+
+if ! hash git 2>/dev/null; then
+	echo "Err: Git is not installed" >&2
+	exit 1
+fi
+
+pushd . >/dev/null
+cd "$tmuxdir/plugins"
+
+git clone https://github.com/tmux-plugins/tmux-yank.git
+
+popd >/dev/null
