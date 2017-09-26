fe4a1a25b1c61e3b292609468d215cc038bc0c4f
ocaballeror <ocaballeror@gmail.com>
2017-09-26 14:31:33 +0200

    Added key bindings for seeking through music

--- i3/scripts/music_control.sh
+++ i3/scripts/music_control.sh
@@ -4,40 +4,83 @@ help="Control the current music player.
 Usage: $(basename $0) [play|pause|stop|next|previous|help]
 "
 
+pause() {
+	case $player in
+		"cmus")         cmus-remote --pause;; 		# -u
+		"clementine")   clementine --play-pause;; 	# -t
+		"amarok")       amarok --play-pause;; 		# -t
+		"playerctl")    playerctl pause;;
+	esac
+}
+
+stop() {
+	case $player in
+		"cmus")         cmus-remote --stop;; 		# -s
+		"clementine")   clementine --stop;; 		# -s
+		"amarok")       amarok --stop;; 			# -s
+		"playerctl")    playerctl stop;;
+	esac
+}
+
+next() {
+	case $player in
+		"cmus")         cmus-remote --next;; 	 	# -n
+		"clementine")   clementine --next;; 	 	# -f
+		"amarok")       amarok --next;; 			# -f
+		"playerctl")    playerctl next;;
+	esac
+}
+
+prev() {
+	case $player in
+		"cmus")         cmus-remote --prev;; 		# -r
+		"clementine")   clementine --previous;;		# -r
+		"amarok")       amarok --previous;; 		# -r
+		"playerctl")    playerctl previous;;
+	esac
+}
+
+# Accepts a relative offset in seconds (e.g. +5)
+seek() {
+	case $player in
+		"cmus")         cmus-remote --seek $1;;
+		"clementine")   clementine --seek-by $1;;
+	esac
+}
+
+quit() {
+	case $player in
+		"cmus") 		cmus-remote -C quit;;
+		"clementine") 	pkill clementine;;
+		"amarok") 		pkill amarok;;
+	esac
+}
+
 # First try to detect the player
 if hash cmus 2>/dev/null && cmus-remote -Q >/dev/null 2>&1; then
-	pause="cmus-remote --pause" # -u
-	next="cmus-remote --next" 	# -n
-	prev="cmus-remote --prev" 	# -r
-	stop="cmus-remote --stop" 	# -s
+	player=cmus
 elif hash clementine 2>/dev/null && pgrep clementine >/dev/null 2>&1; then
-	pause="clementine --play-pause" 	# -t
-	next="clementine --next"        	# -f
-	prev="clementine --previous"    	# -r
-	stop="clemetine --stop"         	# -s
+	player=clementine
 elif hash amarok 2>/dev/null && pgrep amarok >/dev/null 2>&1; then
-	pause="amarok --play-pause" 	# -t
-	next="amarok --next"        	# -f
-	prev="amarok --previous"    	# -r
-	stop="clemetine --stop"         	# -s
+	player=amarok
 elif hash playerctl 2>/dev/null; then
-	pause="playerctl pause"
-	next="playerctl next"
-	prev="playerctl previous"
-	stop="playerctl stop"
+	player=playerctl
 fi
-
 
 
 case $1 in
 	play|pause|toggle)
-		( $pause );;
+		pause;;
 	stop)
-		( $stop );;
+		stop;;
 	next)
-		( $next );;
+		next;;
 	previous|prev)
-		( $prev );;
+		prev;;
+	seek)
+		seek $2;;
+	quit)
+		quit;;
 	help|-h|--help)
 		echo $help;;
 esac
