7ab779e4f59a67d5faac5e31282023be7076c641
Oscar Caballero <ocaballero@opensistemas.com>
2018-11-12 15:53:55 +0100

    Update config for ptpython>=2.0

--- ptpython/config.py
+++ ptpython/config.py
@@ -5,7 +5,7 @@ Copy this file to ~/.ptpython/config.py
 """
 from __future__ import unicode_literals
 from prompt_toolkit.filters import ViInsertMode
-from prompt_toolkit.key_binding.input_processor import KeyPress
+from prompt_toolkit.key_binding.key_processor import KeyPress
 from prompt_toolkit.keys import Keys
 from pygments.token import Token
 
@@ -103,39 +103,52 @@ def configure(repl):
     # Use this colorscheme for the code.
     repl.use_code_colorscheme('native')
 
-    # Enable 24bit True color. (Not all terminals support this. -- maybe check
-    # $TERM before changing.)
-    repl.true_color = False
+    # Set color depth (keep in mind that not all terminals support true color).
+
+    #repl.color_depth = 'DEPTH_1_BIT'  # Monochrome.
+    #repl.color_depth = 'DEPTH_4_BIT'  # ANSI colors only.
+    repl.color_depth = 'DEPTH_8_BIT'  # The default, 256 colors.
+    #repl.color_depth = 'DEPTH_24_BIT'  # True color.
+
+    # Syntax.
+    repl.enable_syntax_highlighting = True
 
     # Install custom colorscheme named 'my-colorscheme' and use it.
     """
     repl.install_ui_colorscheme('my-colorscheme', _custom_ui_colorscheme)
     repl.use_ui_colorscheme('my-colorscheme')
+    """
 
     # Add custom key binding for PDB.
+    """
     @repl.add_key_binding(Keys.ControlB)
     def _(event):
         ' Pressing Control-B will insert "pdb.set_trace()" '
         event.cli.current_buffer.insert_text('\nimport pdb; pdb.set_trace()\n')
+    """
 
     # Typing ControlE twice should also execute the current command.
     # (Alternative for Meta-Enter.)
+    """
     @repl.add_key_binding(Keys.ControlE, Keys.ControlE)
     def _(event):
         b = event.current_buffer
         if b.accept_action.is_returnable:
             b.accept_action.validate_and_handle(event.cli, b)
+    """
 
 
     # Typing 'jj' in Vi Insert mode, should send escape. (Go back to navigation
     # mode.)
+    """
     @repl.add_key_binding('j', 'j', filter=ViInsertMode())
     def _(event):
         " Map 'jj' to Escape. "
-        event.cli.input_processor.feed(KeyPress(Keys.Escape))
+        event.cli.key_processor.feed(KeyPress(Keys.Escape))
     """
 
     # Custom key binding for some simple autocorrection while typing.
+    """
     corrections = {
         'impotr': 'import',
         'pritn': 'print',
@@ -151,11 +164,13 @@ def configure(repl):
         b = event.cli.current_buffer
         w = b.document.get_word_before_cursor()
 
-        if w is not None and w in corrections:
-            b.delete_before_cursor(count=len(w))
-            b.insert_text(corrections[w])
+        if w is not None:
+            if w in corrections:
+                b.delete_before_cursor(count=len(w))
+                b.insert_text(corrections[w])
 
         b.insert_text(' ')
+    """
 
 
 # Custom colorscheme for the UI. See `ptpython/layout.py` and
