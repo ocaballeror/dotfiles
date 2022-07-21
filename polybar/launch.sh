af20aded485bfc5cc07e4e6f81abfea3cbd49c01
Oscar Caballero <oscar.caballero@bayes.gg>
2022-07-21 14:30:21 +0200

    Add polybar config

---
+++ polybar/launch.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+polybar-msg cmd quit
+
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload example &
+done
