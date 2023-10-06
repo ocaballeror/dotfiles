f06ababb4c53f09e921e82f1fc5d25135a0ead0e
Oscar Caballero <oscar.caballero@bayes.gg>
2023-10-06 12:04:33 +0200

    Improve cmp completion

--- neovim/lua/plugins.lua
+++ neovim/lua/plugins.lua
@@ -279,6 +279,7 @@ require("lazy").setup({
             'hrsh7th/cmp-path',
             'hrsh7th/cmp-cmdline',
             'hrsh7th/nvim-cmp',
+            'hrsh7th/cmp-nvim-lsp-signature-help',
 
             'L3MON4D3/LuaSnip',
             'saadparwaiz1/cmp_luasnip',
@@ -324,10 +325,13 @@ require("lazy").setup({
                         end
                     end,
                 },
-                sources = {
+                sources = cmp.config.sources({
                     { name = 'nvim_lsp' },
                     { name = 'luasnip' },
-                },
+                    { name = 'nvim_lsp_signature_help' },
+                }, {
+                    { name = 'buffer' },
+                })
             }
         end
     },
