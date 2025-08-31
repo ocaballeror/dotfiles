e8631d8dc1e8965abbe87812fec402d079f5a11c
Oscar Caballero <ocaballeror@tutanota.com>
2025-08-31 19:06:00 +0200

    Add dunst config

---
+++ dunst/scripts/meet.sh
@@ -0,0 +1,1 @@
+firefox --new-window meet.google.com &
---
+++ dunst/scripts/thing.sh
@@ -0,0 +1,5 @@
+rm -f /home/oscar/thing
+env >> /home/oscar/thing
+pwd >> /home/oscar/thing
+whoami >> /home/oscar/thing
+echo $0 >> /home/oscar/thing
