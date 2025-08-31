5889a3858440ff8648f8adef5236b9e2f3d24105
Oscar Caballero <ocaballeror@tutanota.com>
2025-08-31 19:02:18 +0200

    New style lsp config for nvim 0.11+

--- neovim/lua/lsp.lua
+++ neovim/lua/lsp.lua
@@ -1,49 +1,83 @@
 require('plugins')
 
-local nvim_lsp = require 'lspconfig'
-local telescope = require 'telescope.builtin'
-local on_attach = function(client, bufnr)
-    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+local servers = {
+    clangd = {
+        cmd = { "clangd" },
+        filetypes = { 'c', 'cpp' },
+    },
+    solargraph = {
+        cmd = { "solargraph", "stdio" },
+        filetypes = { 'ruby' },
+    },
+    bashls = {
+        cmd = { "bash-language-server", "start" },
+        filetypes = { 'sh' },
+    },
+    tsserver = {
+        cmd = { "typescript-language-server", "--stdio" },
+        filetypes = { 'typescript', 'javascript' },
+    },
+    lua_ls = {
+        cmd = { "lua-language-server" },
+        filetypes = { 'lua' },
+    },
+    pylsp = {
+        cmd = { "pylsp" },
+        filetypes = { 'python' },
+        settings = {
+            pylsp = {
+                -- configurationSources = { "flake8" },
+                plugins = {
+                    pylsp_mypy = { enabled = true, dmypy = false },
+                    jedi_completion = { fuzzy = true },
+                    ruff = {
+                        enabled = true,
+                        exclude = { ".venv" },
+                        lineLength = 99,
+                        formatEnabled = true,
+                        unsafeFixes = true,
+                        preview = true,
+                    }
+                },
+            },
+        },
+    },
+}
 
-    local opts = { noremap = true, silent = true, buffer=bufnr }
-    vim.keymap.set('n', 'gt', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'gi', telescope.lsp_implementations, opts)
-    vim.keymap.set('n', 'gr', telescope.lsp_references, opts)
-    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-    vim.keymap.set('n', '<leader>di', vim.diagnostic.open_float, opts)
-    -- vim.keymap.set('n', '<leader>bl', vim.lsp.buf.format, opts)
-end
-
--- Add additional capabilities supported by nvim-cmp
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
--- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-local servers = { 'clangd', 'solargraph', 'bashls' }
-for _, lsp in ipairs(servers) do
-    nvim_lsp[lsp].setup {
-        on_attach = on_attach,
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+for name, config in pairs(servers) do
+    vim.lsp.config[name] = {
+        root_markers = { ".git", "pyproject.toml", "package.json", ".luarc.json" },
+        cmd = config.cmd,
+        filetypes = config.filetypes,
+        settings = config.settings,
         capabilities = capabilities,
     }
+    vim.lsp.enable(name)
 end
 
-nvim_lsp.pylsp.setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-    settings = {
-        pylsp = {
-            configurationSources = 'flake8',
-            plugins = {
-                flake8 = { enabled = false, maxLineLength = 99 },
-                pyflakes = { enabled = false },
-                pycodestyle = { enabled = false },
-                pylint = { enabled = false },
-                pylsp_mypy = { enabled = true, dmypy = false },
-                pyls_isort = { enabled = false },
-                jedi_completion = { fuzzy = true },
-                -- black = { enabled = true, cache_config = true, line_length = 99 },
-                ruff = { enabled = true, exclude = { ".venv" }, maxLineLength = 99 },
-            }
-        },
-    }
-}
+local telescope = require 'telescope.builtin'
+vim.api.nvim_create_autocmd('LspAttach', {
+    callback = function(ev)
+        local client = vim.lsp.get_client_by_id(ev.data.client_id)
+        local opts = { noremap = true, silent = true, buffer = ev.buf }
+
+        vim.keymap.set('n', 'gt', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+        vim.keymap.set('n', 'gi', telescope.lsp_implementations, opts)
+        vim.keymap.set('n', 'gr', telescope.lsp_references, opts)
+        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+        vim.keymap.set('n', '<leader>di', vim.diagnostic.open_float, opts)
+        vim.keymap.set('n', '<leader>bl', vim.lsp.buf.format, opts)
+
+        if client.supports_method('textDocument/completion') then
+            vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+        end
+    end
+})
+
+vim.opt.completeopt = { 'menu', 'popup', 'noselect', 'fuzzy' }
+vim.diagnostic.config({
+    virtual_text = true,
+    -- virtual_lines = true,
+})
